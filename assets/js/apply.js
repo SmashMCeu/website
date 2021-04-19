b4b03355b99ea0c0a3ca3ac5c1acc1e531662f68
Andreas <andreas.gutmann01@gmail.com>
2021-04-19 11:15:20 +0200

    added general files

---
+++ assets/js/apply.js
@@ -0,0 +1,68 @@
+﻿$(document).ready(function () {
+    $('select').formSelect();
+});
+
+$("#apply").on("submit", (e) => {
+    let missing = false;
+    let checkboxFields = $("div[data-required-field]");
+
+    for (let i = 0; i < checkboxFields.length; i++) {
+        let cur = checkboxFields[i];
+        let attr = $(cur).attr("data-required-field");
+        let valid = true;
+
+        switch (attr) {
+            case "checkbox":
+                let inputs = $(cur).find("input[type='checkbox']");
+                let isAnyChecked = false;
+                for (let inp of inputs) {
+                    let checked = $(inp).prop("checked");
+                    if (checked) {
+                        isAnyChecked = true;
+                    }
+                }
+                if (!isAnyChecked) {
+                    valid = false;
+                    let spans = $(inputs).siblings("span");
+                    setWrong(spans); 
+                    $(inputs).on("change", function () {
+                        $(spans).removeClass("wrong");
+                    });
+                }
+                break;
+            case "select":
+                let select = $(cur).find("select");
+                let value = $(select).val();
+                if (value == null) {
+                    valid = false;
+                    let item = $(select).siblings("input")[0];
+                    setWrong(item);
+                    $(select).on("change", function () {
+                        if ($(this).val() != null) {
+                            $(item).removeClass("wrong");
+                        }
+                    });
+                }
+                break;
+        }
+
+        if (!valid) {
+            missing = true;
+            break;
+        }
+    }
+
+    if (missing) {
+        e.preventDefault();
+        return false;
+    }
+});
+
+function setWrong(item) {
+    if (item != null) {
+        $(item).addClass("wrong");
+        $("html, body").animate({
+            scrollTop: $(item).offset().top - 50
+        }, 500);
+    }
+}
---
+++ assets/php/sendForm.php
@@ -0,0 +1,25 @@
+﻿<?php
+
+$categorie = isset($_REQUEST['categorie']) ? $_REQUEST['categorie'] : '';
+$firstname = isset($_REQUEST['firstname']) ? $_REQUEST['firstname'] : '';
+$lastname = isset($_REQUEST['lastname']) ? $_REQUEST['lastname'] : '';
+$email = isset($_REQUEST['email']) ? $_REQUEST['email'] : '';
+$subject = isset($_REQUEST['subject']) ? $_REQUEST['subject'] : '';
+$message = isset($_REQUEST['message']) ? $_REQUEST['message'] : '';
+
+$retval = mail(
+      "support@smashmc.eu", 
+      "Bewerbungseingang - '" . $rank . "'", 
+      "Diese Mail wurde mit dem Kontakt-Formular verschickt.
+      \n\nBewerbungsrang: " . $rank . 
+      "\nVorname: " . $name . 
+      "\nMinecraft-Name: " . $mc_name . 
+      "From: " . "Formular");
+  
+  if ($retval == true) {
+    echo "Success";
+  } else {
+    echo "Failure";
+  }
+
+?>
