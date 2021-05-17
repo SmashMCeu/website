e67c6834e9c1d6c01f08902e6a1a93fda3f3e704
cokejoke <justin@knoter.eu>
2021-05-17 22:43:20 +0200

    :sparkles: Little molecraft secret

--- assets/js/scroll-smooth.js
+++ assets/js/scroll-smooth.js
@@ -6,3 +6,26 @@ $(document).ready(function(){
     document.location.href = String( document.location.href ).replace( "#/", "" );
   });
 });
+
+// Hidden secret
+let i = 0;
+$("#molecraft").click(function () {
+  if(i < 2) {
+    i++;
+    return;
+  }
+  let breakSound = new Audio("assets/js/cringe.wav");
+  $("body").append(
+    '<img id="cringe" src="assets/img/molecraft/item.png" alt="minecraft item" />'
+  );
+  document.onmousemove = function (e) {
+    let el = $("#cringe");
+    el.css("position", "fixed");
+    el.css("top", e.clientY + 10);
+    el.css("left", e.clientX + 10);
+  };
+  $("img, span, p, h1, h4, h5, li, a, button, .alert").click(function () {
+    $(this).remove();
+    breakSound.play();
+  });
+});
