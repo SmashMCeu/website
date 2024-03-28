daa5ac3a10750e64a837296fa8810a2ce639e8b3
Artus <artuslama@gmail.com>
2024-03-28 19:42:53 +0100

    🚧 Update index page

--- components/pageHeader/Home.vue
+++ components/pageHeader/Home.vue
@@ -1,13 +1,14 @@
 <template>
     <PageHeaderBase>
-        <p class="font-black text-white tracking-tight text-4xl md:text-6xl">SmashMC</p>
-        <p class="font-semibold text-textColorLight md:ml-2 mt-2 text-lg md:text-2xl tracking-tight leading-5 md:leading-7">
-            SmashMC offers unique games you can play with your friends. Take a look at our Games or just join the minecraft server and experience it yourself!
-        </p>
-        <div class="mt-4 w-fit flex flex-col sm:flex-row gap-4 items-center">
-            <CardsMinecraftServerIp />
-            <CardsDiscord />
+        <div class="-mt-10 md:mt-0 flex flex-col items-center md:items-start">
+            <p class="font-black text-white tracking-tight text-4xl md:text-6xl text-center md:text-start">SmashMC</p>
+            <p class="font-semibold text-textColorLight md:ml-2 mt-2 text-lg md:text-2xl tracking-tight leading-5 md:leading-7 text-center md:text-start">
+                SmashMC offers unique games you can play with your friends. Take a look at our Games or just join the minecraft server and experience it yourself!
+            </p>
+            <div class="mt-4 w-fit flex flex-col sm:flex-row gap-4 items-center">
+                <CardsMinecraftServerIp />
+                <CardsDiscord />
+            </div>
         </div>
-        
     </PageHeaderBase>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,17 @@
     <div>
         <PageHeaderHome />
 
-        <MapsCarousel class="mt-20 max-w-[55rem]"/>
+        <div class="p-10 md:p-20">
 
+            <div class="w-full flex flex-col items-center">
+                <div class="flex flex-col items-center">
+                    <p class="text-xl tracking-tight text-primary-300">Maps</p>
+                    <p class="text-2xl md:text-3xl font-bold text-center">Take a look at our maps</p>
+                </div>
+                <MapsCarousel class="mt-5 md:mt-10 max-w-[55rem]"/>
+                <UButton variant="outline" icon="i-heroicons-chevron-right" trailing size="lg" class="mt-8" to="maps">All Maps</UButton>
+            </div>
+        </div>
     </div>
 </template>
 <script setup lang="ts">
