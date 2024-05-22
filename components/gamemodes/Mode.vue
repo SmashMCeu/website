80c30f29f9ce73932429218336cf32411ffbd045
Artus <artuslama@gmail.com>
2024-05-22 18:33:52 +0200

    add smash gamemode border glow

--- components/gamemodes/Mode.vue
+++ components/gamemodes/Mode.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
 
-        <div class="flex bg-darkBackground rounded-lg overflow-hidden items-center" :class='{ "border-primary border shadow-sm shadow-primary-600": props.border }'>
+        <div class="flex bg-darkBackground rounded-lg overflow-hidden items-center" :class='{ "colored-border": props.border }'>
             <img :src='"./images/gamemodes/" + $props.iconFileName' class="size-28 sm:size-32 md:size-40 ">
 
             <div class="px-4 sm:px-10 md:px-12">
@@ -29,3 +29,42 @@
     });
 
 </script>
+<style scoped>
+
+    .colored-border {
+        
+        animation: borderAnimation 10s infinite;
+
+    }
+
+    @keyframes borderAnimation {
+        0% {
+            -webkit-box-shadow: 0px 0px 10px 0px rgba(255,0,255,1);
+            -moz-box-shadow: 0px 0px 10px 0px rgba(255,0,255,1);
+            box-shadow: 0px 0px 10px 0px rgba(255,0,255,1);
+        }
+        25% {
+            -webkit-box-shadow: 0px 0px 10px 0px rgb(0, 195, 255);
+            -moz-box-shadow: 0px 0px 10px 0px rgb(0, 195, 255);
+            box-shadow: 0px 0px 10px 0px rgb(0, 195, 255);
+        }
+        50% {
+            -webkit-box-shadow: 0px 0px 10px 0px rgb(0, 255, 55);
+            -moz-box-shadow: 0px 0px 10px 0px rgb(0, 255, 55);
+            box-shadow: 0px 0px 10px 0px rgb(0, 255, 55);
+        }
+        75% {
+            -webkit-box-shadow: 0px 0px 10px 0px rgb(0, 195, 255);
+            -moz-box-shadow: 0px 0px 10px 0px rgb(0, 195, 255);
+            box-shadow: 0px 0px 10px 0px rgb(0, 195, 255);
+        }
+        100% {
+            -webkit-box-shadow: 0px 0px 10px 0px rgba(255,0,255,1);
+            -moz-box-shadow: 0px 0px 10px 0px rgba(255,0,255,1);
+            box-shadow: 0px 0px 10px 0px rgba(255,0,255,1);
+        }
+    }
+
+    
+
+</style>
