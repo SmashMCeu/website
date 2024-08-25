eaffdd96ae27b5c63bad3fcd140369349329111e
ArtusLama <artuslama@gmail.com>
2024-08-25 13:56:40 +0200

    :sparkles: RadixUI support (radix-vue)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,5 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   tailwindcss: { viewer: false },
   
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss', 'radix-vue/nuxt']
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
     <div>
         <p class="text-4xl text-primary">Hello!</p>
+        <SwitchRoot v-model:checked="switchState" class="w-[42px] h-[25px] flex bg-black/50 shadow-sm rounded-full relative data-[state=checked]:bg-black">
+            <SwitchThumb class="block w-[21px] h-[21px] my-auto bg-primary shadow-sm rounded-full transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]"/>
+        </SwitchRoot>
     </div>
 </template>
+<script setup lang="ts">
+
+    const switchState = ref(false)
+
+</script>
