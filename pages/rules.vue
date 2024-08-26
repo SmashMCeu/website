1c0207d494da9a15ebe1a3e9f5747d5b9ad0f656
ArtusLama <artuslama@gmail.com>
2024-08-26 22:29:57 +0200

    :sparkles: add Rules

---
+++ pages/rules.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+
+        <Markdown path="https://smashmceu.github.io/website-content/rules/de.md" />
+
+    </div>
+</template>
+<script lang="ts" setup>
+
+    definePageMeta({
+        layout: "markdown",
+    });
+    
+</script>
