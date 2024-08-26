d7c21d27fd6cde03ba0e5a964e3ff2dacbebd17e
ArtusLama <artuslama@gmail.com>
2024-08-26 17:23:56 +0200

    :goal_net: Error Page

---
+++ error.vue
@@ -0,0 +1,23 @@
+<template>
+    <Navbar />
+    <div class="min-h-screen w-screen flex flex-col items-center justify-center">
+        <div class="flex flex-col items-center">
+            <p class="text-textColorLight md:text-8xl sm:text-7xl text-4xl font-black">{{ error?.statusCode }}</p>
+            <p class="md:text-3xl text-xl font-semibold">{{ error?.message }}</p>
+        </div>
+        <button @click="handleError" class="mt-6 px-4 py-2 text-primary flex gap-2 border-2 border-primary rounded-lg">
+            <Icon class="mt-[1px]" size="20" name="solar:home-outline"/>
+            <p>Go Home</p>
+        </button>
+    </div>
+    <Footer />
+</template>
+<script lang="ts" setup>
+    import type { NuxtError } from '#app'
+
+    const props = defineProps({
+        error: Object as () => NuxtError
+    })
+
+    const handleError = () => clearError({ redirect: '/' })
+</script>
