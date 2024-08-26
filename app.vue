b60f6696da63de53de2e1331dc677883d55bdb25
ArtusLama <artuslama@gmail.com>
2024-08-26 13:44:29 +0200

    :bug: ios zoom bug

--- app.vue
+++ app.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <Navbar />
-    <NuxtPage />
-  </div>
+    <div>
+        <NuxtRouteAnnouncer />
+        <Navbar />
+        <NuxtPage />
+    </div>
 </template>
+<style>
+
+    /* Fix IOS zoom on click bug */
+    :root { 
+        touch-action: pan-x pan-y;
+    }
+    
+</style>
