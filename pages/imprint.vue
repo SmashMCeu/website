c073c65e29e3e0254495dc1ca275d227d4ecb746
ArtusLama <artuslama@gmail.com>
2024-08-26 22:29:45 +0200

    :truck: make imprint use new markdown component

--- pages/imprint.vue
+++ pages/imprint.vue
@@ -1,25 +1,14 @@
 <template>
     <div>
 
-        <MDCRenderer v-if="md" :data="md.data" :body="md.body" />
-        <h1 v-else>Loading...</h1>
+        <Markdown local path="imprint.md" />
 
     </div>
 </template>
 <script lang="ts" setup>
+
     definePageMeta({
         layout: "markdown",
     });
-
-
-    import type { MDCParserResult } from '@nuxtjs/mdc';
-
-
-    const md: Ref<MDCParserResult | null> = ref(null);
-
-    onBeforeMount(async () => {
-        const rawContent = await $fetch('/md/imprint.md');        
-        md.value = await parseMarkdown(rawContent as string);
-    });
-
+    
 </script>
