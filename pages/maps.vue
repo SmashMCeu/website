701675acaf2c9f59dd93a27963271ab51806cb2f
ArtusLama <artuslama@gmail.com>
2024-08-27 08:52:02 +0200

    :lipstick: Prettier MapList

--- pages/maps.vue
+++ pages/maps.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="grid grid-cols-3 gap-6">
-        <div v-for="map of maps" class="relative flex items-center justify-center">
-            <img class="rounded-xl" :src="useSmashMaps().getSmashMapImage(map.image)" :alt="map.image">
-            <p class="absolute bottom-2 text-black/75 text-lg font-semibold">{{ map.name }}</p>
+        <div v-for="map of maps" class="relative flex items-center justify-center group">
+            <img class="group-hover:brightness-50 group-hover:scale-105 transition-all rounded-xl" :src="useSmashMaps().getSmashMapImage(map.image)" :alt="map.image">
+            <p class="group-hover:scale-150 group-hover:opacity-100 opacity-0 transition-all absolute text-textColorLight text-lg font-semibold">{{ map.name }}</p>
         </div>
     </div>
 </template>
