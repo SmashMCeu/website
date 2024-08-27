d3f2a0d8235543d0afe0090119e820ee70eb5826
ArtusLama <artuslama@gmail.com>
2024-08-27 08:53:56 +0200

    :iphone: Responsive MapList Grid

--- pages/maps.vue
+++ pages/maps.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="grid grid-cols-3 gap-6">
+    <div class="grid xl:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6">
         <div v-for="map of maps" class="relative flex items-center justify-center group">
             <img class="group-hover:brightness-50 group-hover:scale-105 transition-all rounded-xl" :src="useSmashMaps().getSmashMapImage(map.image)" :alt="map.image">
             <p class="group-hover:scale-150 group-hover:opacity-100 opacity-0 transition-all absolute text-textColorLight text-lg font-semibold">{{ map.name }}</p>
