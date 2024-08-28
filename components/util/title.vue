9d0a337a87b5bbfe87f563aa938f5e13662c00bc
ArtusLama <artuslama@gmail.com>
2024-08-28 16:47:29 +0200

    :bug: title align fix

--- components/util/title.vue
+++ components/util/title.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="flex flex-col" :class="orientation == 'left' ? 'items-start' : (orientation == 'middle' ? 'items-center' : 'items-end')">
         <p class="text-primary font-bold tracking-wide">{{ subtitle }}</p>
-        <p class="text-textColorLight text-3xl font-bold">{{ title }}</p>
+        <p class="text-textColorLight text-3xl font-bold"
+            :class="orientation == 'left' ? 'text-start' : (orientation == 'middle' ? 'text-center' : 'text-end')">
+            {{ title }}
+        </p>
     </div>
 </template>
 <script lang="ts" setup>
