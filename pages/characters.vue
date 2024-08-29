0afefa3c81ba7193bc75ba8b5b9edd3c4cbd3dab
ArtusLama <artuslama@gmail.com>
2024-08-29 13:53:52 +0200

    :lipstick: Skeleton loading indicator for characters list

--- pages/characters.vue
+++ pages/characters.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
-        <CharactersGird />
+        <Suspense>
+            <template #default>
+                <CharactersGird />
+            </template>
+            <template #fallback>
+                <div class="grid lg:grid-cols-4 sm:grid-cols-3 grid-cols-2 sm:gap-8 gap-4">
+                    <div v-for="n of 10" class="bg-backgroundColorLight rounded-lg animate-pulse lg:h-[19rem] sm:h-[17rem] h-[14rem]"></div>
+                </div>
+            </template>
+        </Suspense>
     </div>
 </template>
 <script lang="ts" setup>
