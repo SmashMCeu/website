2a47d83c15008728114afb5a79f08c818a2708d2
ArtusLama <artuslama@gmail.com>
2024-08-29 17:24:17 +0200

    :recycle: Split maps loading into components & skeleton loading

---
+++ components/maps/Grid.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="grid xl:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6">
+        <MapsGridItem v-for="map of maps" :map="map" />
+    </div>
+</template>
+<script lang="ts" setup>
+
+    const maps: Ref<SmashMap[]> = ref(await useSmashMaps().getSmashMaps());
+
+</script>
--- pages/maps.vue
+++ pages/maps.vue
@@ -1,13 +1,14 @@
 <template>
-    <div class="grid xl:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6">
-        <div v-for="map of maps" class="relative flex items-center justify-center group">
-            <img class="bg-backgroundColorLight w-full aspect-video group-hover:brightness-50 group-hover:scale-105 transition-all rounded-xl" :src="useSmashMaps().getSmashMapImage(map.image)" :alt="map.image"/>
-            <p class="group-hover:scale-150 group-hover:opacity-100 opacity-0 transition-all absolute text-textColorLight text-lg font-semibold">{{ map.name }}</p>
-        </div>
+    <div>
+        <Suspense>
+            <template #default>
+                <MapsGrid />
+            </template>
+            <template #fallback>
+                <div class="grid xl:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6">
+                    <div v-for="n of 10" class="bg-backgroundColorLight rounded-lg animate-pulse aspect-video"></div>
+                </div>
+            </template>
+        </Suspense>
     </div>
 </template>
-<script lang="ts" setup>
-
-    const maps: Ref<SmashMap[]> = ref(await useSmashMaps().getSmashMaps());
-
-</script>
