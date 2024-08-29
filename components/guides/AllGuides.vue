b5b5b35fc97ee1895ecc7d9b48ec8e7767e7f251
ArtusLama <artuslama@gmail.com>
2024-08-29 15:52:53 +0200

    :recycle: Put guide loading into composable

--- components/guides/AllGuides.vue
+++ components/guides/AllGuides.vue
@@ -2,7 +2,7 @@
     <div class="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
         <NuxtLink v-for="guide of guides.concat(guides.toReversed())" :to="'/guides/' + guide.url_id">
             <div class="bg-backgroundColorLight h-full hover:scale-[103%] transition-transform p-4 cursor-pointer rounded-lg flex flex-col items-center">
-                <div :style="{ backgroundImage: `url('${getThumbnailImageUrl(guide)}')` }" class="bg-cover bg-center rounded-lg w-full h-[13rem]"></div>
+                <div :style="{ backgroundImage: `url('${useSmashGuides().getGuideThumbnailUrl(guide)}')` }" class="bg-cover bg-center rounded-lg w-full h-[13rem]"></div>
                 <div class="px-4 py-3 w-full">
                     <p class="text-textColorLight md:text-2xl text-xl font-semibold">{{ guide.title }}</p>
                     <p class="text-textColor line-clamp-3 md:text-base text-sm break-before-auto">{{ guide.description }}</p>
@@ -13,38 +13,6 @@
 </template>
 <script lang="ts" setup>
 
-    const pb = useNuxtApp().$pocketbase;
-
-    function getThumbnailImageUrl(guide: GuidesEntry): string {
-        return pb.files.getUrl({
-            collectionId: guide.collectionId,
-            id: guide.id
-        }, guide.thumbnail);
-    }
-
-
-    const guides: Ref<GuidesEntry[]> = ref([]);
-
-
-    // Nuxt error handling...
-    const { data, error } = await useAsyncData("guides", async () => {
-        try {
-            return await pb.collection("active_guides").getFullList<GuidesEntry>({ sort: "title" });
-        } catch (e) {
-            showError({
-                statusCode: 500,
-                message: "Failed to fetch guides",
-            });
-        }
-    });
-
-    if (error.value) {
-        throw createError({
-            statusCode: 500,
-            message: "Failed to fetch guides",
-        });
-    } else if (data.value) {
-        guides.value = data.value;
-    }
+    const guides: Ref<GuidesEntry[]> = ref(await useSmashGuides().getGuidesList());
     
 </script>
---
+++ composables/useSmashGuides.ts
@@ -0,0 +1,37 @@
+import PocketBase from "pocketbase";
+
+export const useSmashGuides = () => {
+
+    // vs doesn't always import the type correctly...
+    const pb: PocketBase = useNuxtApp().$pocketbase as PocketBase;
+    
+    const activeGuidesCollectio = "active_guidess";
+
+    
+    async function getGuidesList(): Promise<GuidesEntry[]> {
+        try {
+            return await pb.collection(activeGuidesCollectio).getFullList<GuidesEntry>({ sort: "title" });
+        } catch (error) {
+            createError({
+                statusCode: 500,
+                message: "Failed to fetch guides",
+            });
+            showError({
+                statusCode: 500,
+                message: "Failed to fetch guides",
+            });
+            return [];
+        }
+    }
+
+    function getGuideThumbnailUrl(guide: GuidesEntry): string {
+        return pb.files.getUrl({
+            collectionId: guide.collectionId,
+            id: guide.id
+        }, guide.thumbnail);
+    }
+
+
+    return { getGuidesList, getGuideThumbnailUrl };
+
+}
