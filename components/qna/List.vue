3fa530049fcb3461233cc180e8c65d16586bcd8b
ArtusLama <artuslama@gmail.com>
2024-08-30 19:39:11 +0200

    :iphone: Make QnA-Accordion more responsive

--- components/qna/List.vue
+++ components/qna/List.vue
@@ -10,7 +10,7 @@
                     :value="item.value"
                 >
                     <AccordionHeader class="flex">
-                        <AccordionTrigger class="rounded data-[state=open]:text-primary text-textColorLight hover:bg-backgroundColorLight/75 transition-colors duration-300 flex flex-1 cursor-pointer items-center gap-2 bg-backgroundColorLight py-5 px-5 text-[15px] leading-none outline-none group">
+                        <AccordionTrigger class="md:text-base text-sm text-start rounded data-[state=open]:text-primary text-textColorLight hover:bg-backgroundColorLight/75 transition-colors duration-300 flex flex-1 cursor-pointer items-center gap-2 bg-backgroundColorLight md:py-5 py-4 md:px-5 px-3 text-[15px] leading-none outline-none group">
                             <Icon
                                 name="solar:alt-arrow-down-outline"
                                 class="mt-[2px] size-5 text-green10 ease-[cubic-bezier(0.87,_0,_0.13,_1)] transition-transform duration-300 group-data-[state=open]:rotate-180"
@@ -19,7 +19,7 @@
                         </AccordionTrigger>
                     </AccordionHeader>
                     <AccordionContent class="text-textColor bg-mauve2 data-[state=open]:animate-accordionSlideDown data-[state=closed]:animate-accordionSlideUp overflow-hidden text-[15px]">
-                        <div class="px-5 py-4 markdown *:!m-0" v-html="item.content"></div>
+                        <div class="md:px-5 px-2 md:py-4 py-2 markdown *:!m-0" v-html="item.content"></div>
                     </AccordionContent>
                 </AccordionItem>
             </template>
