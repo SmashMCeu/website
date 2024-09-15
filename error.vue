dacd3151cd29811e5ce646243faa4f5d5d5bd851
ArtusLama <artuslama@gmail.com>
2024-09-15 14:25:58 +0200

    :goal_net: Improved error page

--- error.vue
+++ error.vue
@@ -3,9 +3,10 @@
     <div class="min-h-screen w-screen flex flex-col items-center justify-center">
         <div class="flex flex-col items-center">
             <p class="text-textColorLight md:text-8xl sm:text-7xl text-4xl font-black">{{ error?.statusCode }}</p>
-            <p class="md:text-3xl text-xl font-semibold">{{ error?.message }}</p>
+            <p class="md:text-3xl text-xl font-semibold text-center">{{ error?.message }}</p>
         </div>
-        <button @click="handleError" class="mt-6 px-4 py-2 text-primary flex gap-2 border-2 border-primary rounded-lg">
+        <button @click="handleError" class="mt-6 px-4 py-2 text-primary flex gap-2 border-2 border-primary rounded-lg
+                                            hover:bg-primary hover:text-backgroundColor hover:scale-110 transition-all">
             <Icon class="mt-[1px]" size="20" name="solar:home-outline"/>
             <p>Go Home</p>
         </button>
