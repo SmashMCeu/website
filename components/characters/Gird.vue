0588be2ed9b9d4d316ba8d8302e12ca076df0f3a
ArtusLama <artuslama@gmail.com>
2024-09-17 20:40:04 +0200

    :lipstick: Better Characters grid alignment

--- components/characters/Gird.vue
+++ components/characters/Gird.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="grid container xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 sm:gap-8 gap-4">
+    <div class="grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
         
         <CharactersGridItem v-for="char of characters" :char="char"/>
         
--- components/characters/GridItem.vue
+++ components/characters/GridItem.vue
@@ -1,13 +1,11 @@
 <template>
     <CharactersCharacterInfo :character="char">
-        <div class="bg-backgroundColorLight rounded-lg center-content hover:scale-105 transition-transform cursor-pointer
-            p-4
-            flex flex-col gap-2">
-            <div class="flex flex-col items-center w-full">
-                <p v-if="char.isPro" class="w-fit px-[8px] pt-[2px] pb-[3px] text-sm text-primary border-2 border-primary rounded-lg font-semibold">PRO</p>
-                <p class="text-base sm:text-lg md:text-xl font-semibold truncate">{{ char.name }}</p>
+        <div class="bg-backgroundColorLight hover:scale-105 transition-transform rounded-lg w-full h-full p-4 flex flex-col items-center justify-between gap-4">
+            <div class="flex flex-col items-center w-full h-full justify-end">
+                <p v-if="char.isPro" class="w-fit px-[6px] py-[1px] text-sm text-primary border-2 border-primary rounded-lg font-semibold">PRO</p>
+                <p class="text-base sm:text-lg md:text-xl font-semibold w-full truncate">{{ char.name }}</p>
             </div>
-            <UtilSkinRenderer2d :skin-url="useSmashCharacters().getCharacterImgUrl(char, char.preview)" />
+            <UtilSkinRenderer2d class="" :skin-url="useSmashCharacters().getCharacterImgUrl(char, char.preview)" />
         </div>
     </CharactersCharacterInfo>
 </template>
