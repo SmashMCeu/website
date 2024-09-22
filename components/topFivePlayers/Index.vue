f01d45c4102bc9e489f0de80cc3da4d07f42bfda
Artus <artuslama@gmail.com>
2024-09-22 19:52:10 +0200

    :lipstick: Responsive top-5 display

--- components/topFivePlayers/Index.vue
+++ components/topFivePlayers/Index.vue
@@ -1,6 +1,6 @@
 <template>
-   <div class="flex justify-center flex-shrink bg-backgroundColorLight px-8 py-4 rounded-lg">
-        <div class="flex flex-col items-center gap-4" v-for="(player, index) of topFivePlayers" :key="player.uuid">
+   <div class="flex flex-wrap justify-center gap-4">
+        <div class="rounded-lg bg-backgroundColorLight py-4 flex flex-col items-center gap-4" v-for="(player, index) of topFivePlayers" :key="player.uuid">
             <p class="text-xl font-semibold text-nowrap">{{ index + 1 }}. {{ player.name }}</p>
             <LazySkinRenderer :skin-image="player.texture" :is-slim="player.slimSkin" static :userRotate="false" />            
         </div>
@@ -13,7 +13,7 @@ const loading = ref(true)
     const limit: number = 5;
     const topFivePlayers: Ref<BasicMinecraftUser[]> = ref<BasicMinecraftUser[]>([]);
 
-    const { data: response   } = await useFetch<TopStatsList>(`/api/stats/top?limit=${limit}`);
+    const { data: response } = await useFetch<TopStatsList>(`/api/stats/top?limit=${limit}`);
     
     if (response.value) {
         let promise = Promise.all(
