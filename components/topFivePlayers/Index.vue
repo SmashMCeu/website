edc66c979db2abfb0d55eea14d4b5e7ce6315a27
cokejoke <justin@knoter.eu>
2024-09-25 18:52:28 +0200

    :lipstick: Adjust Top 5 Card size

--- components/topFivePlayers/Index.vue
+++ components/topFivePlayers/Index.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="flex flex-wrap justify-center gap-4">
-        <div data-aos="fade-up" :data-aos-delay="50*index"  class="w-48 rounded-lg bg-backgroundColorLight py-4 flex flex-col items-center gap-4" v-for="(player, index) of topFivePlayers" :key="player.uuid">
-            <p class="sm:text-xl text-base font-semibold text-nowrap">{{ index + 1 }}. {{ player.name }}</p>
+        <div data-aos="fade-up" :data-aos-delay="50*index"  class="w-full max-w-52 rounded-lg bg-backgroundColorLight py-4 flex flex-col items-center gap-4" v-for="(player, index) of topFivePlayers" :key="player.uuid">
+            <p class="sm:text-lg text-base font-semibold text-nowrap">{{ index + 1 }}. {{ player.name }}</p>
             <!--<LazySkinRenderer :skin-image="player.texture" :is-slim="player.slimSkin" static :userRotate="false" />-->
             <SkinVZGERenderer2d :playerUUID="player.uuid" class="p-4"/>
         </div>
