884857eb6183c72f5fe0a58bfb267e5446600784
Artus <artuslama@gmail.com>
2024-09-25 18:33:57 +0200

    :lipstick: Improved top-5 cards size & alignment

--- components/topFivePlayers/Index.vue
+++ components/topFivePlayers/Index.vue
@@ -1,15 +1,13 @@
 <template>
    <div class="flex flex-wrap justify-center gap-4">
-        <div data-aos="fade-up" :data-aos-delay="50*index"  class="rounded-lg bg-backgroundColorLight py-4 flex flex-col items-center gap-4" v-for="(player, index) of topFivePlayers" :key="player.uuid">
-            <p class="text-xl font-semibold text-nowrap">{{ index + 1 }}. {{ player.name }}</p>
+        <div data-aos="fade-up" :data-aos-delay="50*index"  class="w-48 rounded-lg bg-backgroundColorLight py-4 flex flex-col items-center gap-4" v-for="(player, index) of topFivePlayers" :key="player.uuid">
+            <p class="sm:text-xl text-base font-semibold text-nowrap">{{ index + 1 }}. {{ player.name }}</p>
             <!--<LazySkinRenderer :skin-image="player.texture" :is-slim="player.slimSkin" static :userRotate="false" />-->
-            <SkinVZGERenderer2d :playerUUID="player.uuid" class="w-[80%] h-[80%]"/>            
+            <SkinVZGERenderer2d :playerUUID="player.uuid" class="p-4"/>
         </div>
     </div> 
 </template>
 <script lang="ts" setup>
-
-const loading = ref(true)
 
     const limit: number = 5;
     const topFivePlayers: Ref<BasicMinecraftUser[]> = ref<BasicMinecraftUser[]>([]);
@@ -22,7 +20,7 @@ const loading = ref(true)
                 await useBasicMinecraftUser().uuidToBasicUser(uuid)
             )
         )
-        topFivePlayers.value = await promise
+        topFivePlayers.value = await promise;
     }
         
 
