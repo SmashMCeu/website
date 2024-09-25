55411c3a3ed1bb7b1d4fce29e100478cbaa72e81
Artus <artuslama@gmail.com>
2024-09-25 20:48:10 +0200

    :lipstick: Prettier IP and Discord card

--- components/socialCard/Base.vue
+++ components/socialCard/Base.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-fit px-2 py-2 pr-6 rounded-lg flex gap-2 bg-backgroundColor/50 hover:bg-backgroundColor/75 transition-colors">
+    <div class="w-fit px-2 py-2 pr-6 rounded-lg flex gap-2 bg-backgroundColor/75 hover:bg-backgroundColor/85 backdrop-blur-sm transition-colors">
         <div class="w-14 h-14 center-content">
             <slot name="icon"/>
         </div>
