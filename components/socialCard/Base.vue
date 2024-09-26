b05a2d145f71e9907a18c31a44e0f72d7f1ae675
Artus <artuslama@gmail.com>
2024-09-26 15:51:37 +0200

    :lipstick: Better SocialCards background color

--- components/socialCard/Base.vue
+++ components/socialCard/Base.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-fit px-2 py-2 pr-6 rounded-lg flex gap-2 bg-backgroundColor/75 hover:bg-backgroundColor/85 backdrop-blur-sm transition-colors">
+    <div class="w-fit px-2 py-2 pr-6 rounded-lg flex gap-2 bg-backgroundColor/65 hover:bg-backgroundColor/80 backdrop-blur-sm transition-colors">
         <div class="w-14 h-14 center-content">
             <slot name="icon"/>
         </div>
