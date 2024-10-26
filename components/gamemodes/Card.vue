5e6ff4b61225bce46dc4a4ad3dbaca776d8dd7ab
Artus <artuslama@gmail.com>
2024-10-26 13:46:08 +0200

    :zap: lazy load gamemode images

--- components/gamemodes/Card.vue
+++ components/gamemodes/Card.vue
@@ -1,8 +1,8 @@
 <template>
     <div data-aos="zoom-in" class="rounded-lg bg-backgroundColorLight w-96">
         <div class="relative rounded-lg overflow-hidden select-none pointer-events-none">
-            <img :src="useSmashGamemodes().getGamemodeBackgroundUrl(gamemode)" class="rounded-t-lg brightness-50 blur-[1px]" />
-            <img :src="useSmashGamemodes().getGamemodeIconUrl(gamemode)" class="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-32"/>
+            <img loading="lazy" :src="useSmashGamemodes().getGamemodeBackgroundUrl(gamemode)" class="rounded-t-lg brightness-50 blur-[1px]" />
+            <img loading="lazy" :src="useSmashGamemodes().getGamemodeIconUrl(gamemode)" class="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-32"/>
         </div>
         <div class="px-6 py-4">
             <p class="text-textColorLight font-semibold text-2xl">{{ gamemode.name }}</p>
