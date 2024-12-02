ab075c280be8cdcd6d2aed31a86a70d5fd9d5ce8
Artus <artuslama@gmail.com>
2024-12-02 17:50:45 +0100

    :lipstick: Add tooltip for advanced item tag

--- components/items/AdvancedBadge.vue
+++ components/items/AdvancedBadge.vue
@@ -1,3 +1,12 @@
 <template>
-    <span class="bg-red-500 rounded text-sm font-semibold px-3 flex items-center pb-1 pt-[2px]">Erweitertes Item</span>
+    <TooltipProvider>
+        <TooltipRoot :delay-duration="500">
+            <TooltipTrigger>
+                <span class="bg-cyan-600 rounded text-sm font-semibold px-3 flex items-center pb-1 pt-[2px]">Erweitertes Item</span>
+            </TooltipTrigger>
+            <TooltipContent side="bottom" align="start">
+                <p class="bg-backgroundColorLight brightness-200 rounded-lg px-2 py-1 text-sm text-textColor font-normal">Erweiterte Items können von Pro-Spielern für eine Runde aktiviert werden.</p>
+            </TooltipContent>
+        </TooltipRoot>
+    </TooltipProvider>
 </template>
