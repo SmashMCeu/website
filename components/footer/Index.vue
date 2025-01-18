6c7f64df1cf7b97135532d0eb3ed122260370724
Artus <artuslama@gmail.com>
2025-01-18 21:21:18 +0100

    :bookmark: Update footer license to 2025

--- components/footer/Index.vue
+++ components/footer/Index.vue
@@ -6,7 +6,7 @@
                 <img class="w-10 hover:animate-spin" src="/img/logo.svg" alt="logo">
                 <div>
                     <p class="text-textColorLight font-black text-2xl">SMASHMC.EU</p>
-                    <p class="text-textColor/75 leading-3">© 2017 - 2024</p>
+                    <p class="text-textColor/75 leading-3">© 2017 - 2025</p>
                 </div>
             </div>
             <p class="max-w-[25rem] mt-4">We are not affiliated with Mojang or Microsoft</p>
